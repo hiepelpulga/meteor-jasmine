63b6290c9adecf2d4a13327ac135297de529fa14
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2015-09-23 14:30:24 +0200

    Updates test-app to METEOR@1.2.0.1

--- test-app/packages/package-to-test/package.js
+++ test-app/packages/package-to-test/package.js
@@ -11,7 +11,7 @@ Package.describe({
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.1.0.2');
+  api.versionsFrom('1.2.0.1');
   api.addFiles('package-to-test.js');
   api.export('PackageToTest');
 });
