770c13e6b1350ecf22dbc5d0b6f97a1e67e3e147
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2015-09-25 13:45:34 +0200

    Run client tests after server tests (fixes #280)
    For package testing.

--- src/client/integration/clientsideSetup.js
+++ src/client/integration/clientsideSetup.js
@@ -1,4 +1,23 @@
 var testFramework = new ClientIntegrationTestFramework()
 Meteor.startup(function () {
-  testFramework.runTests()
+  Meteor.call('jasmine/environmentInfo', function (error, mirrorInfo) {
+    if (error) {
+      log.error('Could not get environment info', error);
+      return;
+    }
+
+    if (mirrorInfo.isTestPackagesMode) {
+      Tracker.autorun(function () {
+        var serverAggregateReport = Velocity.Collections.AggregateReports
+          .findOne({name: 'jasmine-server-integration'});
+
+
+        if (serverAggregateReport && serverAggregateReport.result === 'completed') {
+          testFramework.runTests();
+        }
+      });
+    } else {
+      testFramework.runTests()
+    }
+  });
 })
